4cde02d5d6c2a51d7a2f6e80a1f7094c5e0f9cd0
Rajeev <rajgupta123@gmail.com>
2022-07-08 13:28:39 +0530

    Add project files.

---
+++ Controllers/MyEmpController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication53_CallApi.Controllers
+{
+    public class MyEmpController : Controller
+    {
+        public IActionResult showAllEMployee()
+        {
+            return View();
+        }
+    }
+}
